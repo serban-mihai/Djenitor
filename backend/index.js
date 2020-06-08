c9d41f9678ea14282959773fff64fc1817d2cebb
serban-mihai <mihai.ciprian.serban@gmail.com>
2020-06-08 16:59:09 +0300

    EXPRESS | Enstablished a successfull connection between React and Express

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const port = process.env.PORT || 5000;
+
+const router = require("./router")
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const TEENSY_PORT = "/dev/ttyACM0";
+const BAUDRATE = 2000000;
+
+const SerialPort = require("serialport");
+const serial = new SerialPort(TEENSY_PORT, { baudRate: BAUDRATE });
+
+const Readline = require('@serialport/parser-readline');
+const parser = new Readline();
+serial.pipe(parser);
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    parser.on("data", (line) => {
+        socket.emit("notes", JSON.parse(line));
+    });
+
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+
+app.use(router);
+server.listen(port, () => console.log(`Server Started on port ${port}`));
