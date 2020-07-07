73351e397b726c3595e572102d9b98c97214c390
serban-mihai <mihai.ciprian.serban@gmail.com>
2020-07-07 14:29:37 +0300

    ELECTRON | Packaged the App into Electron

---
+++ client/src/components/ControlPanel.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import MyButton from "../custom/Button";
+
+const ControlPanel = (props) => {
+
+    const listInstruments = () => {
+        
+    }
+
+    return (
+        <div className="ControlPanel-Outer">
+          <MyButton name="Test"></MyButton> 
+        </div>
+      );
+};
+
+export default ControlPanel;
